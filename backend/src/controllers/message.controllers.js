61ced127941df48064bc54f7773979059b339cd1
Shreyas Gore <shreyas24087@iiitd.ac.in>
2025-06-06 22:22:07 +0530

    Updated message.controllers.js - message encryption

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -2,79 +2,87 @@ import cloudinary from "../lib/cloudinary.js";
 import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import { encrypt, safeDecrypt } from "../lib/utils.js"; 
 
-export const getUsersForSidebar = async(req,res) =>{
-     try{
+export const getUsersForSidebar = async (req, res) => {
+    try {
         const loggedInUserId = req.user._id;
-        const filteredUsers = await User.find({_id: {$ne:loggedInUserId}}).select("-password");
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-        res.status(200).json(filteredUsers)
-     }
-     catch(error)
-     {
-        console.error("Error in getUsersForSidebar: ",error.message);
-        res.status(500).json({message: "Internal Server Error"});
-     }
+        res.status(200).json(filteredUsers);
+    } catch (error) {
+        console.error("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
-export const getMessages= async(req,res) =>{
-     try{
-        const { id:userToChatId } = req.params
-        const myId = req.user._id
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
 
         const messages = await Message.find({
-            $or:[
-                {senderId:myId, receiverId:userToChatId},
-                {senderId:userToChatId,receiverId:myId}
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
             ]
-        })
+        });
 
-        res.status(200).json(messages)
-     }
-     catch(error)
-     {
-        console.error("Error in getMessages controller: ",error.message);
-        res.status(500).json({message: "Internal Server Error"});
-     }
+        // 🔐 Decrypt messages
+        const decryptedMessages = messages.map(msg => ({
+            ...msg._doc,
+            text: safeDecrypt(msg.text)
+        }));
+
+        res.status(200).json(decryptedMessages);
+    } catch (error) {
+        console.error("Error in getMessages controller: ", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
-//message can be text or image
-export const sendMessage= async(req,res) =>{
-   try{
-        const {text,image} = req.body;
-        const {id: receiverId} = req.params;
+// message can be text or image
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
         const senderId = req.user._id;
 
         let imageUrl;
-        if(image)
-        {
-            //upload base64 image to cloudinary
+        if (image) {
+            // upload base64 image to cloudinary
             const uploadResponse = await cloudinary.uploader.upload(image);
             imageUrl = uploadResponse.secure_url;
         }
-        
+
+        // 🔐 Encrypt text message before saving
+        const encryptedText = text ? encrypt(text) : null;
+
         const newMessage = new Message({
             senderId,
             receiverId,
-            text,
+            text: encryptedText,
             image: imageUrl
         });
-        
+
         await newMessage.save();
 
-        //real time messaging using Socket.io will goes from here
+        // 🔐 Send decrypted text via socket
         const receiverSocketId = getReceiverSocketId(receiverId);
-        if(receiverSocketId)
-        {
-            io.to(receiverSocketId).emit("newMessage",newMessage);
+        if (receiverSocketId) {
+            io.to(receiverSocketId).emit("newMessage", {
+                ...newMessage._doc,
+                text: safeDecrypt(newMessage.text)
+            });
         }
 
-
-        res.status(201).json(newMessage)
-   }
-   catch(error)
-   {
-       console.error("Error in sendMessage controller: ",error.message);
-       res.status(500).json({error: "Internal Server Error"});
-   }
+        // 🔐 Send decrypted message to response too
+        res.status(201).json({
+            ...newMessage._doc,
+            text: safeDecrypt(newMessage.text)
+        });
+    } catch (error) {
+        console.error("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 };
