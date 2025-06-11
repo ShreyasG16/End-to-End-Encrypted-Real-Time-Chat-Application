a8203e86a360f48a6b6ab858fbb1670e37378a0f
Shreyas <shreyas24087@iiitd.ac.in>
2025-06-11 12:07:04 +0530

    Allowed trust to proxy

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import {app,server} from "./lib/socket.js"
 
+app.set("trust proxy", 1);
+
 dotenv.config()
 
 const PORT = process.env.PORT;
